443ed12860e4b7708919525892409cb1e58eb1da
Precious Togodui <139689641+Paatogodui1@users.noreply.github.com>
2026-04-10 06:55:33 +0000

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,4 +2,4 @@ const name = "Precious Togodui"
 const age = 21
 const favorite_programming_language = "Python"
 
-console.log("Hello there! I am ",name,",",age,"years old and my go-to progaramming languauge is", favorite_programming_language,".")
+console.log("Hello there! I am ",name,",",age,"years old and my go-to programming languauge is", favorite_programming_language,".")
