ea036a045a874f4c30a81a5c74a3bd57e7cc1d61
Precious Togodui <akofaamaprecious@gmail.com>
2026-04-10 06:47:27 +0000

    Week 1_First Assignment

---
+++ intro.js
@@ -0,0 +1,5 @@
+const name = "Precious Togodui"
+const age = 21
+const favorite_programming_language = "Python"
+
+console.log("Hello there! I am ",name,",",age,"years old and my go-to progaramming languauge is", favorite_programming_language,".")
